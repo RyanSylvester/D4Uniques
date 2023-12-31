ac254c9312526ad64f18c18131fa166a10212391
RyanSylvester <RyanSylvester21@gmail.com>
2023-12-31 13:26:18 -0500

    populated database with uniques

--- base/models.py
+++ base/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=100)
-    category = models.CharField(max_length=30)
     character = models.CharField(max_length=30)
-    created = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=30) 
     isUber = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
 
