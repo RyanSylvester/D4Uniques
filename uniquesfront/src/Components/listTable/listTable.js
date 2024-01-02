e29e9eb6c1b403e68935c457228c4d1fa5ffd7f3
Ryan Sylvester <ryansylvester21@gmail.com>
2024-01-01 22:08:21 -0500

    filter logic now works

--- uniquesfront/src/Components/listTable/listTable.js
+++ uniquesfront/src/Components/listTable/listTable.js
@@ -5,44 +5,102 @@ class ListTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            itemsByCategory: {}
+            items: [],
+            itemsByCategory: {},
+            filter: {
+                character: 'all',
+                season: 'all',
+                showUbers: true,
+                showCompleted: true,
+            },
         };
     }
 
     componentDidMount() {
+        // Fetch items from DB
         fetch('http://127.0.0.1:8000')
             .then(response => response.json())
             .then(data => {
-                // let items = data;
-                let itemsByCategory = {};
-                // console.log(items);            
+                let items = []; 
                 data.forEach(item => {
-                    // Create Catgory if it doesn't exist
-                    if (!itemsByCategory[item.category]) {
-                        itemsByCategory[item.category] = [];
-                    }
-                    // Add item to category
-                    itemsByCategory[item.category].push(item);
+                    items.push(item);
                 });
-                this.setState({itemsByCategory: itemsByCategory})
+                this.setState({items: items})
+                this.updateItems(this.state.filter.character, this.state.filter.season, this.state.filter.showUbers, this.state.filter.showCompleted);
             })
             .catch(error => console.log(error));
     };
-    
+
+    categorizeItems = (items, filter) => {
+        let itemsByCategory = {};
+        console.log(items);
+        items.forEach(item => {
+            let isCorrectCharacter = (item.character === filter.character || filter.character === "all" || item.character === "all");
+            let isCorrectSeason = (item.season === filter.season || filter.season === "all");
+            let isHidden = (!filter.showUbers && item.uber) || (!filter.showCompleted && item.completed)
+            console.log(item.name, isCorrectCharacter, isCorrectSeason, !isHidden);
+            if (isCorrectCharacter && isCorrectSeason && !isHidden) {
+                // Create Catgory if it doesn't exist
+                if (!itemsByCategory[item.category]) {
+                    itemsByCategory[item.category] = [];
+                } 
+                // Add item to category
+                itemsByCategory[item.category].push(item); 
+            }
+        });
+        return itemsByCategory;
+    };
+
+    updateItems = (newCharacter, newSeason, newUberState, newCompleteState) => {
+        const newFilter = {
+                character: newCharacter,
+                season: newSeason,
+                showUbers: newUberState,
+                showCompleted: newCompleteState,
+            };
+            const newItemsByCategory = this.categorizeItems(this.state.items, newFilter);
+            this.setState({
+                filter: newFilter,
+                itemsByCategory: newItemsByCategory,
+            });
+    };
+
+
 
     render() {
+        const categoryMap = {
+            'helm': 'Helm',
+            'chest': 'Chest',
+            'gloves': 'Gloves',
+            'pants': 'Pants',
+            'boots': 'Boots',
+            'weapon': 'Weapons & Shields',
+            'jewelery': 'Jewelry',
+        };
+
         return (
             <div className="uniquesTable">
-                <p>100%</p>
-                <div className="ProgressBar"/>
+                <div className="tableHeader">
+                    <div className="ProgressVis">
+                        <div className="Progress">100%</div>
+                        <div className="ProgressBar"/>
+                    </div>
+                    <div className="filterBar">
+                        <div className="filterDropdown"></div>
+                        <div className="filterDropdown"></div>
+                        <div className="filterToggle"></div>
+                        <div className="filterToggle"></div>
+                    </div>
+                </div>
+                
                 <div className="listContainer">
-                    <Checklist title="Helm" items={this.state.itemsByCategory['helm']}/>
-                    <Checklist title="Chest" items={this.state.itemsByCategory['chest']}/>
-                    <Checklist title="Gloves" items={this.state.itemsByCategory['gloves']}/>
-                    <Checklist title="Pants" items={this.state.itemsByCategory['pants']}/>
-                    <Checklist title="Boots" items={this.state.itemsByCategory['boots']}/> 
-                    <Checklist title="Weapons & Shields" items={this.state.itemsByCategory['weapon']}/> 
-                    <Checklist title="Jewelry" items={this.state.itemsByCategory['jewelery']}/>
+                    {Object.keys(categoryMap).map(category => (
+                        <Checklist
+                            key={category}
+                            title={categoryMap[category]}
+                            items={this.state.itemsByCategory[category]}
+                        />
+                    ))}
                 </div>
                 
             </div>
