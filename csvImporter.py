63b775865a1ee34630a42cd2fa2867f162d51116
RyanSylvester <RyanSylvester21@gmail.com>
2024-01-02 16:44:31 -0500

    updated seasons in db

--- csvImporter.py
+++ csvImporter.py
@@ -6,8 +6,11 @@ django.setup()
 from base.models import Item
 
 def import_items_from_csv(csv_file):
+    # Clear all items
+    Item.objects.all().delete()
+    
     # Define the column names
-    column_names = ['name', 'character', 'category', 'isUber']
+    column_names = ['name', 'character', 'category', 'isUber', 'season']
 
     # Read the CSV file into a DataFrame
     df = pd.read_csv(csv_file, names=column_names)
@@ -15,7 +18,7 @@ def import_items_from_csv(csv_file):
     # Iterate over the rows of the DataFrame
     for index, row in df.iterrows():
         # Create a new Item object and save it to the database
-        item = Item(name=row['name'], character=row['character'], category=row['category'], isUber=bool(row['isUber']))
+        item = Item(name=row['name'], character=row['character'], category=row['category'], isUber=bool(row['isUber']), season=row['season'])
         item.save()
 
 # Usage
